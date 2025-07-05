9f7f281ad933d94e16d895a8cc9ef3ff815a0b69
SPKALYAN86 <sukalyanpanda07@gmail.com>
2025-07-05 23:38:21 +0530

    Create first.java

---
+++ first.java
@@ -0,0 +1,36 @@
+class Student
+{
+    int roll;
+    String name;
+    double cgpa=cgpa;
+    public Student(String name ,int roll , double cgpa)
+     {
+         this.name = name;
+         this.roll = roll;
+         this.cgpa = cgpa;
+     }
+}
+ class main{
+    public static void main(String[] args) {
+        
+        Student sukalyan = new Student("Sukalyan panda",28,7.0);
+         System.out.println("Details for"+" " +sukalyan.name);
+         System.out.println("name:" +sukalyan.name);
+          System.out.println("roll:"+sukalyan.roll);
+           System.out.println("cgpa:"+sukalyan.cgpa);
+           
+           Student abhishek = new Student("Abhishek",13,8.0);
+            System.out.println("Details for"+ " " +abhishek.name);
+            System.out.println("name:"+abhishek.name);
+            System.out.println("roll:"+abhishek.roll);
+            System.out.println("cgpa:"+abhishek.cgpa);
+           
+            Student sayan = new Student("Sayan",27,8.5);
+            System.out.println("Details for"+ " " +sayan.name);
+            System.out.println("name:"+sayan.name);
+          System.out.println("roll:"+sayan.roll);
+           System.out.println("cgpa:"+sayan.cgpa);
+           
+           
+    }
+}
