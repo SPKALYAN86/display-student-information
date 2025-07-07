b9a7a06e44bd9b2acf2ce3c348eac8985fd8daa8
SPKALYAN86 <sukalyanpanda07@gmail.com>
2025-07-07 23:30:32 +0530

    modified

--- first.java
+++ first.java
@@ -1,39 +1,46 @@
+import java.util.*;
 class Student
 {
     int roll;
     String name;
-    double cgpa=cgpa;
-    public Student(String name ,int roll , double cgpa)
-     {
-         this.name = name;
-         this.roll = roll;
-         this.cgpa = cgpa;
-     }
-}
- class main{
-    public static void main(String[] args) {
-
-
-        // user input for all the parameters
-        // cgpa via array
-        Student sukalyan = new Student("Sukalyan panda",28,7.0);
-         System.out.println("Details for"+" " +sukalyan.name);
-         System.out.println("name:" +sukalyan.name);
-          System.out.println("roll:"+sukalyan.roll);
-           System.out.println("cgpa:"+sukalyan.cgpa);
-           
-           Student abhishek = new Student("Abhishek",13,8.0);
-            System.out.println("Details for"+ " " +abhishek.name);
-            System.out.println("name:"+abhishek.name);
-            System.out.println("roll:"+abhishek.roll);
-            System.out.println("cgpa:"+abhishek.cgpa);
-           
-            Student sayan = new Student("Sayan",27,8.5);
-            System.out.println("Details for"+ " " +sayan.name);
-            System.out.println("name:"+sayan.name);
-          System.out.println("roll:"+sayan.roll);
-           System.out.println("cgpa:"+sayan.cgpa);
-           
-           
+    double cgpa;
+    public Student(String i ,int j , double k)
+    {
+        this.name = i;
+        this.roll = j;
+        this.cgpa = k;
     }
 }
+class main{
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        // user input for all the parameters
+        // cgpa via array
+        String first = scan.nextLine();
+        int second = scan.nextInt();
+        double third= scan.nextDouble();
+
+        Student obj1 = new Student(first,second,third);
+        scan.nextLine();
+
+        String one = scan.nextLine();
+        int two = scan.nextInt();
+        double three= scan.nextDouble();
+        Student obj2 = new Student(one,two,three);
+         scan.nextLine();
+        String num1 = scan.nextLine();
+        int num2 = scan.nextInt();
+        double num3= scan.nextDouble();
+        Student obj3 = new Student(num1,num2,num3);
+          
+        System.out.println("name:"+obj1.name+" "+"roll:"+obj1.roll+" "+"cgpa:"+obj1.cgpa);
+        System.out.println("name:"+obj2.name+" "+"roll:"+obj2.roll+" "+"cgpa:"+obj2.cgpa);
+        System.out.println("cgpa:"+obj3.name+" "+"roll:"+obj3.roll+" "+"cgpa:"+obj3.cgpa);
+
+
+    }
+
+}
+
+
